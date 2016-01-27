dd6413ec1a0681aa19eb3c4859ffd1bf231ad8b4
yajie <yajie.yao@imike.com>
2016-01-27 17:53:58 +0800

    global_queue   main_queue  暂停和恢复操作的区别

--- dispatch_source_t暂停和恢复/ViewController.m
+++ dispatch_source_t暂停和恢复/ViewController.m
@@ -31,8 +31,13 @@
     self.timeInterval = 50.0f;
     NSInteger totalTime =  self.timeInterval;
     
+    //根据分派源数据在主线程中更新UI
+//    _processingQueueSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_DATA_ADD, 0, 0,
+//                                                    dispatch_get_main_queue());
+    
+    //因为不是在主线程中创建，所以不要在此更新UI操作。（此种效果不会更新UI效果），如果需要更新，则必须在主线程中
     _processingQueueSource = dispatch_source_create(DISPATCH_SOURCE_TYPE_DATA_ADD, 0, 0,
-                                                    dispatch_get_main_queue());
+                                                    dispatch_get_global_queue(0, 0));
     __block NSUInteger totalComplete = 0;
     dispatch_source_set_event_handler(_processingQueueSource, ^{
         
@@ -43,11 +48,16 @@
         }else{
             self.timeInterval --;
         }
-        _slider.value = totalComplete;
+         dispatch_async(dispatch_get_main_queue(), ^{
+             //回调或者说是通知主线程刷新（如果监视某些类型事件的对象创建在主线程，那么不必再主线程中写，否则更新必须在主线程操作）
+             _slider.value = totalComplete;
+         });
+        
     });
+    
+    
     //分派源创建时默认处于暂停状态，在分派源分派处理程序之前必须先恢复。
     [self resume];
-    
     
     //恢复源后，就可以通过dispatch_source_merge_data向Dispatch Source(分派源)发送事件:
     dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
@@ -68,9 +78,6 @@
     }else{
         [self pause];
     }
-}
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
 }
 
 
